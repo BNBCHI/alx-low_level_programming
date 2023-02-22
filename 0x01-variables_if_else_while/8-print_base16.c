312cb546cd0529159edc3235873343906db997eb
BNBCHI <chidyaongab@gmail.com>
2023-02-22 14:17:50 +0200

    alphabetsoup

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print Hexadecimal values
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char hex;
+
+	for (num  = '0'; num <= '9'; num++)
+	{
+		putchar(num + 0);
+	}
+	for (hex = 'A'; hex <= 'F'; hex++)
+	{	
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
