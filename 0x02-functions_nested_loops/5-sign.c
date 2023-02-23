6a51a0147a1b8428335e0691c0839956c6460b35
BNBCHI <chidyaongab@gmail.com>
2023-02-23 14:46:32 +0200

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - prints + if input is greater than 0
  * prints 0 if is 0 and - if input is less than 0
- * @n - input number
+ * @n: - input number
  * Return: 1 if greater than 0, 0 if is 0, and -1 if less than 0
  */
 int print_sign(int n)
