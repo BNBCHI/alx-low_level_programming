7547f598b0fdf0a163b6f52207259e06621f308f
BNBCHI <chidyaongab@gmail.com>
2023-02-23 16:44:11 +0200

    9timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for(b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			_putchar(a*b + 48);
+			_putchar(a * b);
 		}
 		_putchar('\n');
 	}
