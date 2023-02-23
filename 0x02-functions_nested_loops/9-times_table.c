2f422ce586f15b0c20dd20d7b436592b1a641ff7
BNBCHI <chidyaongab@gmail.com>
2023-02-23 16:37:11 +0200

    9timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * times_table - prints the 9x table
+ * Return: 9 times table
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for(b = 0; b <= 9; b++)
+		{
+			_putchar(a*b + 48);
+		}
+		_putchar('\n');
+	}
+}
