09c5e6801dd35ce4f78d55d99467f0cb1e21d51a
BNBCHI <chidyaongab@gmail.com>
2023-02-26 00:40:45 +0200

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: the base 10 numbers
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
