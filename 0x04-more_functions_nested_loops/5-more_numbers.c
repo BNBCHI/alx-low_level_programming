607f25061a0c47ed18e2744436f866885c73ccdf
BNBCHI <chidyaongab@gmail.com>
2023-02-26 12:45:08 +0200

    PrintLine

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0-14 10 times
+ * Return: list of numbers 0-14 10 times
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar(b );
+			}
+			else
+			{
+				_putchar(b + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - print a line n number of times
+ * or a 0 for another case
+ * @n: the character in ASCII code
+ * Return: The printed line _ n number of times
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 1; a <= n; a++)
+ 	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			continue;
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
