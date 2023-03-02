58f71172f10f7877995ca64442b9964f7f8ccdfe
BNBCHI <chidyaongab@gmail.com>
2023-03-02 23:37:42 +0200

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 
 /**
  * print_rev - prints a string in reverse followed by a new line
- *
+ *@s: string
  * Return: String in reverse.
  */
 void print_rev(char *s)
 {
 	int x, y;
-	x = 0;
-	
+
+	x = 0;	
 	while (*s != '\0')
 	{
 		x++;
