59a6fd52cf07937f4b6ae564c885cd289a70f2ea
BNBCHI <chidyaongab@gmail.com>
2023-03-10 10:48:59 +0200

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** * _strpbrk - Entry point 
+* @s: input 
+* @accept: input 
+* Return: pointer to the first occurrence of any character in accept in the string s, or NULL if not found 
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ('\0');
+}
