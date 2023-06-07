520c3976d2b16eee0ed53c1a30983dfa5a64bbf1
BNBCHI <chidyaongab@gmail.com>
2023-06-07 16:16:23 +0200

    4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion
+ * @x: input base 
+ * @y: input exponent
+ * Return: Result of base input to the power of exponent
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(x, y-1)); 
+}
